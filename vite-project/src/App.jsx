375d3da8590be37936b2458a0ff6f972b3611409
Abhishek Waghmare <abhiwaghmare3210@gmail.com>
2025-01-23 22:03:30 +0530

    Updated App.jsx
    I was create a multiple winCondition functions

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -5,8 +5,18 @@ import { sum } from './helper';
 
 function App() {
 
+  // let winCondition = (ticket) => {
+  //   return sum(ticket) === 15;
+  // }
+
+  // let winCondition = (ticket) => {
+  //   //All numbers are equal to first number 111, 222, 333
+  //   return ticket.every((num)=> num === ticket[0]);
+  // }
+
   let winCondition = (ticket) => {
-    return sum(ticket) === 15;
+    //if first number is = to 0
+    return ticket[0] === 0;
   }
 
   return (
